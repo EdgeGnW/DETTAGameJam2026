027d4e1297fbd54c59aaeb2219766e088aa2f504
EdgeGnW <68284212+EdgeGnW@users.noreply.github.com>
2026-01-17 11:51:52 +0100

    volume sliders change bus volume

--- main_menu/h_slider.gd
+++ main_menu/h_slider.gd
@@ -1,17 +1,19 @@
 extends HSlider
 
 @export var audio_bus_name: String
+var audio_bus_index: int
+
+const SFX_TEST := preload("res://main_menu/test.wav")
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	# var audio_bus_id = AudioServer.get_bus_index()
-	pass
+	var audio_bus_index = AudioServer.get_bus_index(audio_bus_name)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(audio_bus_index, value / 100)
 	print(value)
+	AudioManager.play_sound(SFX_TEST)
