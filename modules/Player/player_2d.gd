bb7aa04f7a016ed8318138dd7396436fe92d2566
Skuldug <117478730+Skuldug@users.noreply.github.com>
2026-01-17 15:14:01 +0100

    Dismounting and mounting with correct collision

--- modules/Player/player_2d.gd
+++ modules/Player/player_2d.gd
@@ -82,6 +82,9 @@ func dismount(jump_start: bool) -> void:
 	await get_tree().create_timer(1).timeout
 	has_mount = false
 	mount = null
+	for i in range (1,get_child_count()-1):
+		if get_child(i).is_class("CollisionShape2D"):
+			remove_child(get_child(i))
 
 
 func _unhandled_input(event: InputEvent) -> void:
