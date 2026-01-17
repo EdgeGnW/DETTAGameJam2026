c8f24bea2aaa9d35bf785735bbbe44f9ab11c4e3
EdgeGnW <68284212+EdgeGnW@users.noreply.github.com>
2026-01-17 11:36:30 +0100

    removed 3D stuff
    also removed process method from main menu

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -10,12 +10,6 @@ func _ready() -> void:
 	setting_menu.visible = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-
-
 func _on_settings_pressed() -> void:
 	main_buttons.visible = false
 	setting_menu.visible = true
--- modules/Test/3D/ui_root.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-@onready var player: Player3D = %Player
-@onready var inventory_dialog: InventoryDialog = %InventoryDialog
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("inventory"):
-		inventory_dialog.open(player.inventory)
