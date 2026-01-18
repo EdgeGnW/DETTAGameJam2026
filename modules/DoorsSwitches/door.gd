f482ea12f3357021a93f17c206feb19cd84bc1c4
EdgeGnW <68284212+EdgeGnW@users.noreply.github.com>
2026-01-18 16:09:45 +0100

    invert door

--- modules/DoorsSwitches/door.gd
+++ modules/DoorsSwitches/door.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 @export var door_index: int
 @export var switch_threshold: int
 @export var stay_once_opened := true
+@export var invert := false
 
 const DOOR_OPEN = preload("uid://1jyumedpehhl")
 
@@ -23,26 +24,50 @@ func _ready():
 	for switch in self.switches:
 		switch.updated.connect(check_switches)
 		
+	if invert:
+		collision_shape_2d.set_deferred("disabled", true)
+		open = true
+		$Closed.hide()
+		$Open.show()
+		
 func check_switches():
 	var sum = 0
 	for switch in switches:
 		sum += int(switch.pressed)
 	#print(sum, switch_threshold, open, sum >= switch_threshold)
 	if sum >= switch_threshold:
-		if not open:
+		if invert:
+			if open:
+				collision_shape_2d.set_deferred("disabled", false)
+				open = false
+				AudioManager.play_sound(DOOR_OPEN)
+				$Closed.show()
+				$Open.hide()
+				if stay_once_opened:
+					for switch in self.switches:
+						switch.press_permanently()
+		else:
+			if not open:
+				collision_shape_2d.set_deferred("disabled", true)
+				open = true
+				AudioManager.play_sound(DOOR_OPEN)
+				$Closed.hide()
+				$Open.show()
+				if stay_once_opened:
+					for switch in self.switches:
+						switch.press_permanently()
+	elif not stay_once_opened:
+		if invert:
 			collision_shape_2d.set_deferred("disabled", true)
 			open = true
-			AudioManager.play_sound(DOOR_OPEN)
 			$Closed.hide()
+			AudioManager.play_sound(DOOR_OPEN)
 			$Open.show()
-			if stay_once_opened:
-				for switch in self.switches:
-					switch.press_permanently()
-	elif open and not stay_once_opened:
-		collision_shape_2d.set_deferred("disabled", false)
-		open = false
-		$Closed.show()
-		AudioManager.play_sound(DOOR_OPEN)
-		$Open.hide()
+		else:
+			collision_shape_2d.set_deferred("disabled", false)
+			open = false
+			$Closed.show()
+			AudioManager.play_sound(DOOR_OPEN)
+			$Open.hide()
 		
 	
