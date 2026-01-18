d1288d966de80a1906c2e429847240edc2b285e3
EdgeGnW <68284212+EdgeGnW@users.noreply.github.com>
2026-01-18 12:33:05 +0100

    fixed audio bugs

--- main_menu/h_slider.gd
+++ main_menu/h_slider.gd
@@ -9,11 +9,11 @@ const SFX_TEST := preload("res://main_menu/test.wav")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var audio_bus_index = AudioServer.get_bus_index(audio_bus_name)
+	audio_bus_index = AudioServer.get_bus_index(audio_bus_name)
 
 
 
-func _on_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(audio_bus_index, value / 100)
-	print(value)
-	AudioManager.play_sound(SFX_TEST)
+func _on_value_changed(v: float) -> void:
+	AudioServer.set_bus_volume_linear(audio_bus_index, v / 100)
+	if audio_bus_name == "SoundEffects":
+		AudioManager.play_sound(SFX_TEST)
