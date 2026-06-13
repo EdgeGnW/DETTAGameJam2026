4efd1eb74bc4d4dcc5aa340fbbc8f7844f62fa07
EdgeGnW <68284212+EdgeGnW@users.noreply.github.com>
2026-06-13 10:45:37 +0200

    Tweened rays
    made it so the rays smoothly tween
    when selected

--- modules/Pascal/rays.gd
+++ modules/Pascal/rays.gd
@@ -2,21 +2,66 @@ extends Node2D
 
 var rays: Array[Line2D]
 
-@export var low_intensity = 0.05
-@export var high_intensity = 0.1
-@export var highlighted_intensity = 0.3
+@export var low_intensity := 10
+@export var low_width := 5
+@export var low_length := 250
+@export var high_intensity := 19
+@export var high_width := 10
+@export var high_length := 500
+@export var highlighted_intensity := 60
+@export var highlighted_width := 20
+@export var highlighted_length := 750
+@export var tween_time := 0.2
 
 var current_highlight: int = 0
+
+var tween: Tween
 
 func _ready() -> void:
 	rays.assign(find_children("*", "Line2D", false, false))
 	print(len(rays))
 	
 func highlight_ray(index: int) -> void:
+
 	if index == current_highlight:
 		return
-	rays[current_highlight].gradient.set_color(0, Color(1, 1, 1, high_intensity))
-	current_highlight = index
-	rays[current_highlight].gradient.set_color(0, Color(1, 1, 1, highlighted_intensity))
+	var tween_length = func(length: int, ray: Line2D) -> void:
+		ray.points[1].x = length
+		
+	var tween_intensity = func(intensity: float, ray: Line2D) -> void:
+		ray.gradient.colors[0].a = intensity/255
+
+	if tween:
+		tween.pause()
+		tween.custom_step(tween_time)
+		tween.kill()
+		
+	tween = create_tween()
+	tween.set_parallel()
 	
+	#reset old ray
+	var old_line = rays[current_highlight]
+	tween.tween_property(old_line, "width", high_width, tween_time)
+	tween.tween_method(tween_length.bind(old_line),
+		highlighted_length,
+		high_length,
+		tween_time)
+	tween.tween_method(tween_intensity.bind(old_line),
+		highlighted_intensity,
+		high_intensity,
+		tween_time)
+	
+	#set new ray
+	current_highlight = index
+	var new_line = rays[current_highlight]
+	tween.tween_property(new_line, "width", highlighted_width, tween_time)
+	tween.tween_method(tween_length.bind(new_line),
+		high_length,
+		highlighted_length,
+		tween_time)
+	tween.tween_method(tween_intensity.bind(new_line),
+		high_intensity,
+		highlighted_intensity,
+		tween_time)
+		
 	
--- modules/Pascal/trail.gd
+++ modules/Pascal/trail.gd
@@ -19,17 +19,17 @@ func _ready():
 func _process(delta):
 	if node2d == null:
 		return
-	var mouse_position = node2d.position#get_viewport().get_mouse_position()
+	var start_position = node2d.position#get_viewport().get_start_position()
 	# Calculate the distance from the last point to the current mouse position
 	if get_point_count() > 0:
 		var last_point = get_point_position(get_point_count() - 1)
-		var distance = mouse_position.distance_to(last_point)
+		var distance = start_position.distance_to(last_point)
 		distance_accum += distance
 	else:
 		distance_accum = point_spacing # Ensure the first point is added
 	# Add a new point if the accumulated distance exceeds the spacing
 	if distance_accum >= point_spacing:
-		add_point(mouse_position)
+		add_point(start_position)
 		distance_accum = 0.0
 		# Remove the oldest point if we exceed the max number of points
 		if get_point_count() > max_points:
